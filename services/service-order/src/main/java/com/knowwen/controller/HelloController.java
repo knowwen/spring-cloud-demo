ed18d8cd710aff85bd15fdd3b11324b7d856aef5
张知文 <zhangzhiwenlx@outlook.com>
2025-08-07 16:35:14 +0800

    配置中心基本用法

--- services/service-order/src/main/java/com/knowwen/controller/HelloController.java
+++ services/service-order/src/main/java/com/knowwen/controller/HelloController.java
@@ -1,10 +1,24 @@
 package com.knowwen.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HelloController {
+
+    @Value("${order.timeout}")
+    String orderTimeout;
+
+    @Value("${order.auto-confirm}")
+    String orderAutoConfirm;
+
+    @GetMapping("config")
+    public String getConfig(){
+        return "order.timeout="+orderTimeout+",order.auto-confirm="+orderAutoConfirm;
+    }
 
     @GetMapping("/")
     public String index() {
