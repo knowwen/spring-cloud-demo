f81622400d7b3b06f54bdb1497e2efa4feffb2f2
张知文 <zhangzhiwenlx@outlook.com>
2025-08-07 13:05:59 +0800

    网关

--- gateway/src/main/java/com/knowwen/GatewayMainApplication.java
+++ gateway/src/main/java/com/knowwen/GatewayMainApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderMainApplication {
-
+public class GatewayMainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMainApplication.class, args);
+        SpringApplication.run(GatewayMainApplication.class, args);
     }
-
 }
---
+++ services/service-order/src/main/java/com/knowwen/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.knowwen.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello Order service";
+    }
+}
