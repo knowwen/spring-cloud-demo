5be30df1cc93d21ba37c25030b2b63a4ab440f94
knowwen <337235669@qq.com>
2025-08-29 14:38:16 +0800

    Java short类型 +1 错误

---
+++ services/service-product/src/test/java/com/knowwen/MainTest.java
@@ -0,0 +1,22 @@
+package com.knowwen;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+        testShort();
+    }
+
+    public static void testShort(){
+        short s = 1;
+        s = s + 1;
+        s += 1;
+    }
+
+    public static void testString(){
+        String hello = "hello";
+        System.out.println(hello.hashCode());
+        hello += " World";
+        System.out.println(hello.hashCode());
+    }
+
+}
